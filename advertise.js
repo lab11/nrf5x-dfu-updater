96f31064a53d557e8d045c5d4ecd14bb79d99215
Neal Jackson <nealjack@umich.edu>
2016-04-23 21:57:33 -0400

    added advertise script to help put s130 into bootloader

---
+++ advertise.js
@@ -0,0 +1,20 @@
+var argv = require('minimist')(process.argv.slice(2));
+var bleno = require('bleno');
+
+var target = Buffer(Buffer(argv.a, 'hex').readUIntLE(0,6).toString(16), 'hex');
+var data = Buffer([0x00, 0xFF, 0xE0, 0x02, 0x16, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+target.copy(data, 6);
+data[0] = data.length - 1;
+
+bleno.on('stateChange', function(state) {
+  // if we want to send a packet to a listener instead of connecting to
+  // peripheral, we need to advertise to device first
+  if (state === 'poweredOn') {
+    bleno.startAdvertisingWithEIRData(data);
+    setTimeout(function () {
+      process.exit();
+    }, 2000);
+  } else {
+    bleno.stopAdvertising();
+  }
+});
